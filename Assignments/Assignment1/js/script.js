dd4247cb28ebc9372fc9d12148e2961f28093bdb
Alvin Yeboah <alvinyeboah5@icloud.com>
2024-09-28 19:17:09 +0000

    Added landing page

--- Assignments/Assignment1/js/script.js
+++ Assignments/Assignment1/js/script.js
@@ -1,4 +1,4 @@
-const images = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg","img/img5.jpg"];
+const images = ["../img/img1.jpg", "../img/img2.jpg", "../img/img3.jpg", "../img/img4.jpg"];
 
 let currentImageIndex = 0;
 const imageElement = document.getElementById("imageSwitcher");
